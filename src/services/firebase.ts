24f881e56e32b032ce12b6bd7338b180ba7171bf
“HackX-IN” <inamul2002@gmail.com>
2024-01-04 15:13:13 +0530

    Secure_Config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2o3SuqJEKgla8FeUIykbaMcDRTJd2aW4",
-  authDomain: "drawingtool-e25a4.firebaseapp.com",
-  projectId: "drawingtool-e25a4",
-  storageBucket: "drawingtool-e25a4.appspot.com",
-  messagingSenderId: "243868532402",
-  appId: "1:243868532402:web:5d9557fb85866eaf94d3ab",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
